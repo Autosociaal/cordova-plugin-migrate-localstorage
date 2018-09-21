97ab6a99ca73029d5cd5ab848fdf9ab2266104ac
hellkith <kvdkolk@live.nl>
2018-09-21 15:51:56 +0200

    Updated to copy files from bad location first

--- src/ios/MigrateLocalStorage.m
+++ src/ios/MigrateLocalStorage.m
@@ -5,22 +5,22 @@
 - (BOOL) copyFrom:(NSString*)src to:(NSString*)dest
 {
     NSFileManager* fileManager = [NSFileManager defaultManager];
-
+    
     // Bail out if source file does not exist
-    if (![fileManager fileExistsAtPath:src]) {
-        return NO;
-    }
-
-    // Bail out if dest file exists
-    if ([fileManager fileExistsAtPath:dest]) {
-        return NO;
-    }
-
-    // create path to dest
-    if (![fileManager createDirectoryAtPath:[dest stringByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil]) {
-        return NO;
-    }
-
+//    if (![fileManager fileExistsAtPath:src]) {
+//        return NO;
+//    }
+//
+//    // Bail out if dest file exists
+//    if ([fileManager fileExistsAtPath:dest]) {
+//        return NO;
+//    }
+//
+//    // create path to dest
+//    if (![fileManager createDirectoryAtPath:[dest stringByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil]) {
+//        return NO;
+//    }
+    
     // copy src to dest
     return [fileManager copyItemAtPath:src toPath:dest error:nil];
 }
@@ -29,28 +29,42 @@
 {
     // Migrate UIWebView local storage files to WKWebView. Adapted from
     // https://github.com/Telerik-Verified-Plugins/WKWebView/blob/master/src/ios/MyMainViewController.m
-
+    
     NSString* appLibraryFolder = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString* original;
+    
 
+    
     if ([[NSFileManager defaultManager] fileExistsAtPath:[appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage/file__0.localstorage"]]) {
         original = [appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage"];
     } else {
         original = [appLibraryFolder stringByAppendingPathComponent:@"Caches"];
     }
-
+    
+    NSString* originalFile =  [appLibraryFolder stringByAppendingPathComponent: @"WebKit/LocalStorage/file__0.localstorage"];
+    NSString* originalFileTarget = [appLibraryFolder stringByAppendingPathComponent:@"Caches/file__0.localstorage"];
+    NSString* originalFileshm =  [appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage/file__0.localstorage-shm"];
+    NSString* originalFileTargetshm = [appLibraryFolder stringByAppendingPathComponent:@"Caches/file__0.localstorage-shm"];
+    NSString* originalFilewal =  [appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage/file__0.localstorage-wal"];
+    NSString* originalFileTargetwal = [appLibraryFolder stringByAppendingPathComponent:@"Caches/file__0.localstorage-wal"];
+    
     original = [original stringByAppendingPathComponent:@"file__0.localstorage"];
-
+    
     NSString* target = [[NSString alloc] initWithString: [appLibraryFolder stringByAppendingPathComponent:@"WebKit"]];
-
+    
 #if TARGET_IPHONE_SIMULATOR
     // the simulutor squeezes the bundle id into the path
     NSString* bundleIdentifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
     target = [target stringByAppendingPathComponent:bundleIdentifier];
 #endif
-
+    
     target = [target stringByAppendingPathComponent:@"WebsiteData/LocalStorage/http_localhost_8080.localstorage"];
-
+    
+    [self copyFrom:originalFile to:originalFileTarget];
+    [self copyFrom:originalFileshm to:originalFileTargetshm];
+    [self copyFrom:originalFilewal to:originalFileTargetwal];
+    
+    
     // Only copy data if no existing localstorage data exists yet for wkwebview
     if (![[NSFileManager defaultManager] fileExistsAtPath:target]) {
         NSLog(@"No existing localstorage data found for WKWebView. Migrating data from UIWebView");
